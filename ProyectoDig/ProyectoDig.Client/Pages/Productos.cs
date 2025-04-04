ad5610435f33c25b04e549344efb9a77955843b8
santogarrido <santocildesgarridoraul@gmail.com>
2025-04-04 08:20:56 +0200

    Agregar archivos de proyecto.

---
+++ ProyectoDig/ProyectoDig.Client/Pages/Productos.cs
@@ -0,0 +1,18 @@
+﻿namespace ProyectoDig.Client.Pages
+{
+    public class Productos
+    {
+        private int Id { get; set; }
+
+        private string Nombre { get; set; }
+
+        private int Cantidad { get; set; }
+
+        public Productos(int id, string nombre, int cantidad)
+        {
+            Id = id;
+            Nombre = nombre;
+            Cantidad = cantidad;
+        }
+    }
+}
---
+++ ProyectoDig/ProyectoDig.Client/Pages/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoDig.Client.Pages
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress(ErrorMessage = "El email es inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        public string Contraseña { get; set; }
+
+        public string Rol { get; set; }
+
+        public Usuario(string nombre, string email, string contraseña, string rol)
+        {
+            Nombre = nombre;
+            Email = email;
+            Contraseña = contraseña;
+            Rol = rol;
+        }
+    }
+}
